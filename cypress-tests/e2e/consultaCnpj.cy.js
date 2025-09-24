bcf7453425badf725bb92190d9adb62fc0c7a137
Diegoindio <dii.gomess@gmail.com>
2025-09-23 22:44:15 -0300

    test(api): add smoke CNPJ spec and ensure CI finds specs

--- cypress-tests/e2e/consultaCnpj.cy.js
+++ cypress-tests/e2e/consultaCnpj.cy.js
@@ -1,5 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Smoke API - BrasilAPI CNPJ', () => {
+  it('retorna 200 para CNPJ válido', () => {
+    cy.request('GET', '/cnpj/v1/27865757000102').then((res) => {
+      expect(res.status).to.eq(200);
+      expect(res.body).to.have.property('razao_social');
+    });
+  });
+});
